59572d76bc231d2ec88ea29e144b0e577f1240a2
Farhodjon001 <shermuhammadfarhod@gmail.com>
2023-09-14 17:52:37 +0500

    stadium views

--- Stadium/views.py
+++ Stadium/views.py
@@ -11,3 +11,41 @@ class CreateStadiumAPIView(generics.CreateAPIView):
     serializer_class = StadiumSerializers
     permission_classes = (ManagerPermission, AdminPermission)
 
+class ListManagerStadiumAPIView(generics.ListAPIView):
+    queryset = Stadium.objects.all()
+    serializer_class = StadiumSerializers
+    permission_classes = (ManagerPermission,)
+
+    def get_queryset(self):
+        user = self.request.user
+        manager= CustomUser.objects.get(id=user.id)
+        return Stadium.objects.filter(manager=manager)
+
+class ListManagerOrders(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializers
+    permission_classes = (ManagerPermission,)
+
+    def get_queryset(self):
+        user=self.request.user
+        stadion=Stadium.objects.get(manager=user.id)
+        return Order.objects.filter(name=stadion.id)
+
+
+class ListEptyStadiumByManager(APIView):
+    def get(self):
+        stadium=Stadium.objects.get(manager=self.request.user.id)
+        orders=Order.objects.filter(name=stadium.id)
+
+
+
+
+# class ListEptyStadium(APIView)
+#     def get(self, *args, **kwargs):
+#         begin_time = kwargs["begin_time"]
+#         end_time = kwargs["end_time"]
+#
+#         oders=Order.objects.filter(begin_time=)
+#         detail_view = Order.objects.filter(name=detail.id)
+#         serializer = OrderSerializers(detail_view, many=True)
+#         return Response(serializer.data)
--- config/settings.py
+++ config/settings.py
@@ -87,21 +87,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('NAME'),
-#         'USER': os.getenv('USER'),
-#         'PASSWORD': os.getenv('PASSWORD'),
-#         'HOST': os.getenv("HOST"),
-#         'PORT': os.getenv("PORT"),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
 }
 
